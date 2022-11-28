ec8168fc5e00af19923b6a9acb0587d90c36b4c7
fayaz <lagharifayazali3@gmail.com>
2022-11-28 21:35:19 +0500

    Multiple Components in Sync

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { Component } from 'react';
 
 import './App.css';
+import Counters from './components/counters';
+import NavBar from './components/navbar';
 
-function App() {
-  return ;
+
+
+class App extends Component {
+ 
+  render() { 
+    return (<React.Fragment>
+      <NavBar/>
+      <main className='container'> <Counters/></main>
+    </React.Fragment>);
+  }
 }
-
+ 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 import Counters from './components/counters';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <Counters/>
+    <App/>
   </React.StrictMode>
 );
 
